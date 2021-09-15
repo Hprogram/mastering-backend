78de725cd287edfe08ead9e597660d10581bf445
HPROGRAM <wjs2096@gmail.com>
2021-09-15 13:49:30 +0900

    서버 코드 이해 및 테스트 완료(파이썬 과의 통신 공부 필요)

--- routes/fileReceiver.js
+++ routes/fileReceiver.js
@@ -1,54 +1,57 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var multer = require('multer');
+var multer = require("multer");
 var fs = require("fs");
 
-
-router.get('/', function (req, res, next) {
-  res.send("test")
+router.get("/", function (req, res, next) {
+  res.send("test");
 });
 
 const upload = multer({
-  storage: multer.memoryStorage()
-}).single('file');
+  storage: multer.memoryStorage(),
+}).single("file");
 
-router.post('/', (req, res) => {
-  upload(req, res, err => {
-    //console.log(req);
-    //if (err) console.log(err);
-    
-    //console.log(req.file);
-    //console.log("file load completed!! need to convert audio file");
-    
-    //Need to Convert Audio file in below
-    //req.file.filename="fixedFile.wav";
-  
-    //res.writeHead(200, {"Content-Type":"audio/wav"});
-    
+router.post("/", (req, res) => {
+  upload(req, res, (err) => {
+    // //console.log(req);
+    // //if (err) console.log(err);
+    // console.log(req.file);
+    // //console.log("file load completed!! need to convert audio file");
+    // //Need to Convert Audio file in below
+    // //req.file.filename="fixedFile.wav";
+    // res.writeHead(200, { "Content-Type": "audio/wav" });
     var zerorpc = require("zerorpc");
-    var client = new zerorpc.Client({ timeout: 600, heartbeatInterval: 120000 });
-    client.connect("tcp://127.0.0.1:4242");
-    console.log("start invoke")
-    client.invoke("getMasteredAudio", req.file.buffer, req.file.originalname, function(error, resFromPython, more) {
-      if (error != null) {
-        console.log(error)
-        res.status(500).send()
-        res.end();
-        return ;
-      }
-      console.log(resFromPython)
-      res.write(new Buffer(resFromPython).toString("base64"))
-      res.end()
-      console.log("finished")
+    var client = new zerorpc.Client({
+      timeout: 600,
+      heartbeatInterval: 120000,
     });
-    // var fs = require('fs');
-    // fs.readFile('./routes/sample_test_eq.wav', function(err,data){
-    //   console.log()
+    client.connect("tcp://localhost:8080");
+    console.log("start invoke");
+    client.invoke(
+      "getMasteredAudio",
+      req.file.buffer,
+      req.file.originalname,
+      function (error, resFromPython, more) {
+        if (error != null) {
+          console.log(error);
+          res.status(500).send();
+          res.end();
+          return;
+        }
+        console.log(resFromPython);
+        res.write(new Buffer(resFromPython).toString("base64"));
+        res.end();
+        console.log("finished");
+      }
+    );
+    // var fs = require("fs");
+    // fs.readFile("./routes/sample_test_eq.wav", function (err, data) {
+    //   console.log();
     //   res.write(new Buffer(data).toString("base64"));
     //   res.end();
     //   console.log("send response!");
     // });
   });
-})
+});
 
 module.exports = router;
