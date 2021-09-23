437b70eb3f793d4865feb01d995ef45ea74072e9
HPROGRAM <wjs2096@gmail.com>
2021-09-23 10:48:28 +0900

    Update : fileReceiver.js multer.single로 버퍼에 업로드 시키는 설명 추가

--- routes/fileReceiver.js
+++ routes/fileReceiver.js
@@ -7,7 +7,8 @@ router.get("/", function (req, res, next) {
   res.send("test");
 });
 
-// multer를 이용해 메모리 저장공간 (버퍼)를 업로드 시킴.
+// multer를 이용해 메모리 저장공간 (버퍼)로 저장 업로드 시킴.
+// single("file") => 해당 공간에 file이라는 이름을 가진 파일을 버퍼 형태로 저장 시키는 것
 const upload = multer({
   storage: multer.memoryStorage(),
 }).single("file");
@@ -32,10 +33,10 @@ router.post("/", (req, res) => {
     });
     // api로 바꾸기 위해서는 파이썬 서버 코드를 수정해야함.
     client.connect("tcp://192.168.0.100:4242"); // 해당 주소를 파이썬 서버로 직접연결
-    
+
     console.log("start invoke");
-    client.invoke( 
-      // 현재 버퍼에 올라가 있는 데이터와 파일의 오리지널 네임을 전송. 
+    client.invoke(
+      // 현재 버퍼에 올라가 있는 데이터와 파일의 오리지널 네임을 전송.
       // 오류가 없다면 반환값을 버퍼에 base64형태로 입력후 반환.
       "getMasteredAudio",
       req.file.buffer,
