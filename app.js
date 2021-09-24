482d2efafe7bddc7612c4e073672a0f22a9a4074
HPROGRAM <wjs2096@gmail.com>
2021-09-24 16:05:43 +0900

    update : body-parser 추가 및 fileReceiver.js 라우터 위치 정리,주석추가

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
+var bodyParser = reqiure("body-parser");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -7,6 +8,8 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var fileReceiver = require("./routes/fileReceiver");
+
+var parser = bodyParser.urlencoded({ extended: false });
 
 var app = express();
 var cors = require("cors");
@@ -20,6 +23,8 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
--- routes/fileReceiver.js
+++ routes/fileReceiver.js
@@ -7,12 +7,7 @@ router.get("/", function (req, res, next) {
   res.send("test");
 });
 
-// multer를 이용해 메모리 저장공간 (버퍼)로 저장 업로드 시킴.
-// single("file") => 해당 공간에 file이라는 이름을 가진 파일을 버퍼 형태로 저장 시키는 것
-const upload = multer({
-  storage: multer.memoryStorage(),
-}).single("file");
-
+// 플러터 웹에서 form data로 보내지 못할 시 해결하기 위한 api (app.js에서 body-Parser를 적용하여야함)
 router.post("/web", (req, res) => {
   let buffer = req.data.bytes;
   let originalname = req.data.originalname;
@@ -45,6 +40,12 @@ router.post("/web", (req, res) => {
     }
   );
 });
+
+// multer를 이용해 메모리 저장공간 (버퍼)로 저장 업로드 시킴.
+// single("file") => 해당 공간에 file이라는 이름을 가진 파일을 버퍼 형태로 저장 시키는 것
+const upload = multer({
+  storage: multer.memoryStorage(),
+}).single("file");
 
 router.post("/", (req, res) => {
   upload(req, res, (err) => {
@@ -97,52 +98,4 @@ router.post("/", (req, res) => {
   });
 });
 
-/*
-router.post("/", (req, res) => {
-  upload(req, res, (err) => {
-    //console.log(req);
-    //if (err) console.log(err);
-
-    //console.log(req.file);
-    //console.log("file load completed!! need to convert audio file");
-
-    //Need to Convert Audio file in below
-    //req.file.filename="fixedFile.wav";
-
-    //res.writeHead(200, {"Content-Type":"audio/wav"});
-
-    var zerorpc = require("zerorpc");
-    var client = new zerorpc.Client({
-      timeout: 600,
-      heartbeatInterval: 120000,
-    });
-    client.connect("tcp://127.0.0.1:4242");
-    console.log("start invoke");
-    client.invoke(
-      "getMasteredAudio",
-      req.file.buffer,
-      req.file.originalname,
-      function (error, resFromPython, more) {
-        if (error != null) {
-          console.log(error);
-          res.status(500).send();
-          res.end();
-          return;
-        }
-        console.log(resFromPython);
-        res.write(new Buffer(resFromPython).toString("base64"));
-        res.end();
-        console.log("finished");
-      }
-    );
-    // var fs = require('fs');
-    // fs.readFile('./routes/sample_test_eq.wav', function(err,data){
-    //   console.log()
-    //   res.write(new Buffer(data).toString("base64"));
-    //   res.end();
-    //   console.log("send response!");
-    // });
-  });
-});
-*/
 module.exports = router;
