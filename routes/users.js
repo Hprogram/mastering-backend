b9f1aa2399f0234c44daa8df41971f21b853af57
Jeon-YoungHo <wjs2096@gmail.com>
2021-10-11 20:51:32 +0900

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// 이후 회원가입 적용시 유저 정보 관련 api
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
